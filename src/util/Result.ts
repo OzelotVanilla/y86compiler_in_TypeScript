258ee8279995873929446c7b050b430898963a66
Ozelot Vanilla <shuihenyun@qq.com>
2023-12-11 23:40:43 +0800

    util - `Result`
    Which is a little rust flavoured.

---
+++ src/util/Result.ts
@@ -0,0 +1,41 @@
+export class Result<OkType, ErrType>
+{
+    private whether_ok: boolean
+    private value: OkType | ErrType
+
+    public static createOk<ValueType, ErrType = any>(value: ValueType): Result<ValueType, ErrType>
+    {
+        return new Result(true, value)
+    }
+
+    public static createErr<ValueType, OkType = any>(value: ValueType): Result<OkType, ValueType>
+    {
+        return new Result(false, value)
+    }
+
+    public isOk(): boolean { return this.whether_ok }
+
+    public isErr(): boolean { return !this.whether_ok }
+
+    public unwarpOk(): OkType
+    {
+        if (this.isErr()) { throw TypeError(`The result is Err.`) }
+
+        return this.value as OkType
+    }
+
+    public unwarpErr(): ErrType
+    {
+        if (this.isOk()) { throw TypeError(`The result is OkS.`) }
+
+        return this.value as ErrType
+    }
+
+    constructor(whether_ok: true, value: OkType)
+    constructor(whether_ok: false, value: ErrType)
+    constructor(whether_ok: boolean, value: OkType | ErrType)
+    {
+        this.whether_ok = whether_ok
+        this.value = value
+    }
+}
